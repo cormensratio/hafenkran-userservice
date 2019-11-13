670f93e967e72d397097f30ed1282d2b2162a6c3
David Schrenk <schrenkdav@fim.uni-passau.de>
2019-11-13 13:24:59 +0100

    EPDS02-33 add comments to JwtAuthentication class

--- src/main/java/de/unipassau/sep19/hafenkran/userservice/config/JwtAuthentication.java
+++ src/main/java/de/unipassau/sep19/hafenkran/userservice/config/JwtAuthentication.java
@@ -1,11 +1,17 @@
 package de.unipassau.sep19.hafenkran.userservice.config;
 
 import de.unipassau.sep19.hafenkran.userservice.dto.UserDTO;
+import lombok.NonNull;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 
+import javax.validation.Valid;
+
+/**
+ * A class for persisting the {@link UserDTO} of the current user in a thread local {@link org.springframework.security.core.context.SecurityContextHolder}
+ */
 public class JwtAuthentication extends AbstractAuthenticationToken {
 
-    JwtAuthentication(UserDTO userDTO) {
+    JwtAuthentication(@NonNull @Valid UserDTO userDTO) {
         super(null);
         super.setAuthenticated(true);
         super.setDetails(userDTO);
